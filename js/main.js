de1bcb21cea213751c09e133b497fc77f13cb6c0
abtevrythng <g.nelson@DT0717.hia.asn.au>
2015-12-17 10:58:42 +1100

    Added autofocus after clearing editor

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var whichSave = "";
 
 if (localStorage.getItem("cleanState") === null || localStorage.getItem("editorState") === null) {
 	localStorage["cleanState"] = JSON.stringify(element.editor);
+	document.querySelectorAll(".trix-content").focus();
 }
 
 
